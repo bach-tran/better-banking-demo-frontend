f18ee1fedb5c48df4cfe6be34645f02e1d0fe870
Bach T <bach_tran@outlook.com>
2022-11-11 14:29:34 -0500

    Add ability to view accounts

--- user-profile.js
+++ user-profile.js
@@ -1,6 +1,19 @@
 const baseUrl = 'http://127.0.0.1:8080';
 
 /*
+    Logout functionality
+*/
+const logoutButtonElement = document.getElementById('logout');
+logoutButtonElement.addEventListener('click', () => {
+    fetch(`${baseUrl}/logout`, {
+        method: 'POST',
+        credentials: 'include'
+    }).then((res) => {
+        window.location.href = 'index.html';
+    })
+});
+
+/*
     Example of using the Fetch API to send HTTP requests from the browser to the backend
 */
 fetch(`${baseUrl}/current-user`, {
@@ -35,14 +48,38 @@ fetch(`${baseUrl}/current-user`, {
     userInfoDivElement.appendChild(p2);
     userInfoDivElement.appendChild(p3);
     userInfoDivElement.appendChild(p4);
+
+    const tableHeadingElement = document.getElementById('table-heading');
+    if (role === 'customer') {
+        tableHeadingElement.innerHTML = 'Your Accounts'
+    } else if (role === 'admin') {
+        tableHeadingElement.innerHTML = 'All Accounts in The System'
+    }
 });
 
-const logoutButtonElement = document.getElementById('logout');
-logoutButtonElement.addEventListener('click', () => {
-    fetch(`${baseUrl}/logout`, {
-        method: 'POST',
-        credentials: 'include'
-    }).then((res) => {
-        window.location.href = 'index.html';
-    })
+/*
+    Display a table of accounts
+    Customer: their own accounts
+    Admin: all accounts
+*/
+fetch(`${baseUrl}/accounts`, {
+    method: 'GET',
+    credentials: 'include'
+}).then((res) => {
+    return res.json();
+}).then((accounts) => {
+    const tbodyElement = document.querySelector('#accounts tbody') // descendant css selector
+    for (account of accounts) {
+        let tr = document.createElement('tr');
+
+        let idTd = document.createElement('td');
+        idTd.innerHTML = account.id;
+        let balanceTd = document.createElement('td');
+        balanceTd.innerHTML = account.balance;
+
+        tr.appendChild(idTd);
+        tr.appendChild(balanceTd);
+
+        tbodyElement.appendChild(tr);
+    }
 });
